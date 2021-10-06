f7561da08868320785baadfe370895b3f2f34355
a-givertzman <a.givertzman@icloud.com>
2021-10-06 08:32:28 +0300

    selectJoin

--- libPHP/mysql_utils.php
+++ libPHP/mysql_utils.php
@@ -274,10 +274,10 @@ function selectJoinData(
         foreach($searchField as $index => $field) {
             if ($index == 0) {
     
-                $query .= "\nWHERE `$tableNmae`.`$field` LIKE '$searchQuery'";
+                $query .= "\nWHERE `$tableName`.`$field` LIKE '$searchQuery'";
             } else {
                 
-                $query .= "\nOR `$tableNmae`.`$field` LIKE '$searchQuery'";
+                $query .= "\nOR `$tableName`.`$field` LIKE '$searchQuery'";
             }
         }
     
