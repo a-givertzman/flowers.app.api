10013a73fffaee3310ac33d4259952a912e8e41d
a-givertzman <a.givertzman@icloud.com>
2021-10-06 22:20:02 +0300

    selectJoin

--- libPHP/mysql_utils.php
+++ libPHP/mysql_utils.php
@@ -284,7 +284,7 @@ function selectJoinData(
         }
     
         // добавляем сортировку к запросу
-        $query .= "\nORDER BY `$tableName`.`$orderField` $order";
+        $query .= "\nORDER BY `$orderField` $order";
 
         // добавляем лимит количества записей в результате
         $query .= ($limit > 0) 
