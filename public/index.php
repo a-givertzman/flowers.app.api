8e0a96561c50b170f3d3807c797e3f9f280c2bf5
a-givertzman <a.givertzman@icloud.com>
2021-10-19 01:35:19 +0300

    payment

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 var_dump($_GET);
 
-if (empty($_GET) || $_GET('url') == '') {
+if (empty($_GET) || $_GET['url'] == '') {
     require_once './clientOverview.php';
     exit;
 }
 
-if ($_GET('url') == 'payment') {
+if ($_GET['url'] == 'payment') {
     require_once './payment.php';
     exit;
 }
