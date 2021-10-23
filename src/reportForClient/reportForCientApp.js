bf735c8105ac0862227c562db43eefac47750da2
a-givertzman <a.givertzman@icloud.com>
2021-10-23 17:38:07 +0300

    payment

--- src/reportForClient/reportForCientApp.js
+++ src/reportForClient/reportForCientApp.js
@@ -149,7 +149,7 @@ class ReportForClient {
         const selectorElem = this.selector?.render();
         const ordersElem = this.orders?.render();
         const transactionsElem = this.transactions?.render();
-        this.containerElem.appendChild(selectorElem);
+        // this.containerElem.appendChild(selectorElem);
         this.containerElem.appendChild(ordersElem);
         // this.containerElem.appendChild(transactionsElem);
         return 0;
