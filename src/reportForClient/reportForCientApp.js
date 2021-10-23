e7f9a22c90f8bc49a6067e587a7e86b8d05020dd
a-givertzman <a.givertzman@icloud.com>
2021-10-23 17:41:46 +0300

    payment

--- src/reportForClient/reportForCientApp.js
+++ src/reportForClient/reportForCientApp.js
@@ -382,7 +382,7 @@ class ApiRequest {
 
     prepareFetchOptions(body) {
         console.log('[ApiRequest.prepareFetchOptions]');
-        return options = {
+        return {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             // mode: 'cors', // no-cors, *cors, same-origin
             // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
