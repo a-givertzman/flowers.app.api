6d8625a23f695af52dc31f5648817909f21f7ba0
a-givertzman <a.givertzman@icloud.com>
2021-10-26 18:06:37 +0300

    payment

--- sql/functionGetRemains.sql
+++
@@ -1,7 +0,0 @@
-CREATE FUNCTION `getRemains`(count INT, purchase_id INT, purchase_content_id INT) RETURNS int(11)
-BEGIN
-RETURN count - (select sum(`count`)
-	from `purchase_member`
-	where `purchase/id` = purchase_id
-	and `purchase_content/id` = purchase_content_id);
-END
