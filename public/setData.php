1ffcb2f7fabb3ba3ba16a63bd1e9cc6635bc8464
a-givertzman <a.givertzman@icloud.com>
2021-11-10 21:51:05 +0300

    ordet

--- public/setData.php
+++ public/setData.php
@@ -24,12 +24,11 @@ plog("-> setData.php");
 require_once './libPHP/mysql_utils.php';
 
 
-// plog('postData');
-// plog($_POST);
-$post = json_decode($_POST);
-$tableName = json_decode($post['tableName']);
-$data = json_decode($post['data']);
-$keys = json_decode($post["keys"]);      // массив названий полей таблицы
+plog('_POST', $_POST);
+
+$tableName = json_decode($_POST['tableName']);
+$data = json_decode($_POST['data']);
+$keys = json_decode($_POST["keys"]);      // массив названий полей таблицы
 
 plog('tableName: ', $tableName);
 plog('field keys: ', $keys);
