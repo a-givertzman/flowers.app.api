ec776c965ee33e491f4292ddbc9287671d82c9ca
a-givertzman <a.givertzman@icloud.com>
2021-11-10 23:06:44 +0300

    ordet

--- public/setData.php
+++ public/setData.php
@@ -52,30 +52,23 @@ if (!empty($data)) {
 
 // проверяем были ли ошибки и передаем данные вызвавшей форме
 if ($errCount == 0) {
-
     // plog("reporting:");
     // plog($data_id);
-
     // возвращаем id сохраненного элемента
     $jsonText = array(                                                      // формируем набор данных и информацию об ошибках
         'data' => $data_id, // массив id сохраненных записей
         'errCount' => $errCount,
         'errDump' => $errDump
     );
-
     echo json_encode($jsonText);
 } else {
-
     plog("Server reply error: $errDump");
-
     // сообщаем информацию о технической проблеме
     $jsonText = array(
         'errCount' => $errCount,
         'errDump' => $errDump
     );
-
     echo json_encode($jsonText);
 }
-
 
 plog("setData.php ->");
