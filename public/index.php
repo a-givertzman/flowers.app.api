127116b499979d69a42f4796d8efbec41a8be8f9
a-givertzman <a.givertzman@icloud.com>
2021-11-12 12:56:12 +0300

    backend api

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
-
-error_log("\t Before plog", 0);
-
 require_once './libPHP/plog.php';
-error_log("\t After plog", 0);
 plog("-> router.php");
 // plog('_REQUEST: ', $_REQUEST);
 // plog('_SERVER: ', $_SERVER);
@@ -14,35 +10,30 @@ $query = trim($_SERVER['QUERY_STRING'], '/');
 plog('query:', $query);
 
 if ($query == '') {
-    // $html = require_once './clientOverview.php';
-    require_once './reportForClient.php';
-    exit;
-}
-if ($query == 'admin-client-report') {
-    require_once './reportForClient.php';
-    exit;
+    $routePath = './reportForClient.php';
+
+} else if ($query == 'admin-client-report') {
+    $routePath = './reportForClient.php';
+
+} else if ($query == 'payment') {
+    $routePath = './payment.php';
+
+} else if ($query == 'get-data') {
+    $routePath = './api/getData.php';
+
+} else if ($query == 'get-join-data') {
+    $routePath = './api/getJoinData.php';
+
+} else if ($query == 'get-view') {
+    $routePath = './api/getView.php';
+
+} else if ($query == 'set-data') {
+    $routePath = './api/setData.php';
+} else {
+    $routePath = './reportForClient.php';
 }
 
-if ($query == 'payment') {
-    require_once './payment.php';
-    exit;
-}    
-
-if ($query == 'get-data') {
-    require_once './getData.php';
-    exit;
-}
-if ($query == 'get-join-data') {
-    require_once './getJoinData.php';
-    exit;
-}
-if ($query == 'get-view') {
-    require_once './getView.php';
-    exit;
-}
-if ($query == 'set-data') {
-    require_once './setData.php';
-    exit;
-}
+plog('route to: ', $routePath);
+require_once $routePath;
 
 plog("router.php ->");
