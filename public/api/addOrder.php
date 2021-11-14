54564260a52767dadaa20c1c0de3a42529e4a001
a-givertzman <a.givertzman@icloud.com>
2021-11-14 15:20:52 +0300

    backend api

--- public/api/addOrder.php
+++ public/api/addOrder.php
@@ -46,15 +46,12 @@ if (!empty($data)) {
             $clientId = $dataSet['client/id'];
             $productId = $dataSet['product/id'];
             // делаем запрос для проверки существует ли запись с указанными id
-            $query = "SELECT 
-            EXISTS(
-                SELECT * 
+            $query = "SELECT * 
                     FROM `$tableName` 
                     WHERE `purchase/id` = $purchaseId 
                     and `client/id` = $clientId 
                     and `product/id` = $productId 
-                    LIMIT 1
-            );";
+                    LIMIT 1;";
             // plog('updating');
             $current_id = insertOdkuData($tableName, $dataSet, $query);
             array_push($data_id, $current_id);
